416ff8305d3751edd5e307db960cfc36477d6d9d
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-02 19:37:37 +0545

    Dependecy For DB Connection

--- FastApi/main.py
+++ FastApi/main.py
@@ -5,8 +5,30 @@ from pydantic import BaseModel
 from database import SessionLocal, engine
 import models
 from fastapi.middleware.cors import CORSMiddleware
+
 # Learn CORS
 
 app = FastAPI()
 origins = []
 app.add_middleware(CORSMiddleware, allow_origins=origins)
+
+class TransactionBase(BaseModel):
+    amount:float
+    category:str
+    description:str
+    is_income:bool
+    date:str
+
+class TransactionModel(TransactionBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()

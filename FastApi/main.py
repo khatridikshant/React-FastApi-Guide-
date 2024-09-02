644c9ab1acd5e208b8138109db2ab142cbc31b95
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-02 19:44:52 +0545

    Wrote some comments

--- FastApi/main.py
+++ FastApi/main.py
@@ -8,10 +8,17 @@ from fastapi.middleware.cors import CORSMiddleware
 
 # Learn CORS
 
+#Initializes FastAPI application
 app = FastAPI()
+
+#List of allowed origins for CORS
 origins = []
+
+# Add CORS middleware to the FastApi application
 app.add_middleware(CORSMiddleware, allow_origins=origins)
 
+
+# Base Model for transactions using pydantic
 class TransactionBase(BaseModel):
     amount:float
     category:str
@@ -23,12 +30,22 @@ class TransactionModel(TransactionBase):
     id: int
 
     class Config:
-        orm_mode = True
+        orm_mode = True # Allows compatibility with SQLAlchemy models
 
 
+
+# Dependency function to provide database session
 def get_db():
-    db = SessionLocal()
+    db = SessionLocal() # This creates new database session
     try:
-        yield db
+        yield db # Yields the session to be used by dependencies
     finally:
-        db.close()
+        db.close() #Ensures the session is closed after use
+
+
+db_dependecy = Annotated[Session, Depends(get_db)]
+
+# Creates db tables based on SQLAlchemy models
+models.Base.metadata.create_all(bind = engine)
+
+

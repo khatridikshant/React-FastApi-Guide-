1043a47b96d350d88747d1834017d9eb4ce80df8
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-02 19:25:21 +0545

    Transactions Table Created

--- FastApi/models.py
+++ FastApi/models.py
@@ -1,2 +1,13 @@
 from database import Base
 from sqlalchemy import Column, Integer, Boolean, String, Float
+
+class Transaction(Base):
+    __tablename__ = 'transactions'
+
+    id = Column(Integer,primary_key=True, index=True)
+    amount = Column(Float)
+    category = Column(String)
+    description = Column(String)
+    is_income = Column(Boolean)
+    date = Column(String)
+

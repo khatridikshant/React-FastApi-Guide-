a52cde6bda46549cd8ea99fc8ca8cf300e254e05
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-04 17:31:33 +0545

    React Page begins

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -13,15 +13,57 @@ function App() {
     date: ''
 
   });
+    
 
+  const fetchTransactions = async () => {
+    const response = await api.get('/transactions/')
+    setTransactions(response.data)
+  }
+
+  useEffect(() => { fetchTransactions(),[]})
+
+  const handleInputChange = (event) => {
+    const value = event.target.type == 'checkbox' ? event.target.checked : event.target.value;
+    setFormData({
+      ...formData,[event.target.name] : value,
+    });
+  }
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    await api.post('/transactions/', formData);
+    fetchTransactions()
+    setFormData({
+      amount: '',
+      category: '',
+      description: '',
+      is_income: false,
+      date: ''
+    })
+  };
 
 
   return (
-    <>
-<div>
+    <div>
+      <nav className='navbar navbar-dark bg-primary'>
+        <div className="container-fluid">
+          <a className='navbar-brand' href='#'>
+            Finance Application
+          </a>
+        </div>
 
-</div>
-    </>
+      </nav>
+      <div className='container'>
+
+        <form onSubmit={handleFormSubmit}>
+          <div className='mb-3 mt-3'>
+
+          </div>
+
+        </form>
+
+      </div>
+    </div>
   )
 }
 
